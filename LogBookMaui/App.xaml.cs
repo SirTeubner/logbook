c11b526b0496df72380ef8712de044ee0e4f8919
Noah Teubner <nteubner@schueler.hakzell.at>
2024-01-09 10:52:31 +0100

    init commit

---
+++ LogBookMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LogBookMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
