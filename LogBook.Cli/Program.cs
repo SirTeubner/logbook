1fbe687ffc4d274f0e5df77b03d58c8d82bce81f
Noah Teubner <nteubner@schueler.hakzell.at>
2024-02-01 13:07:52 +0100

    add demo data

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -3,6 +3,17 @@
 Console.WriteLine("Willkommen beim Fahrtenbuch!");
 
 IRepository repository = new MemoryRepository();
+
+repository.Add(new Entry(DateTime.Now, DateTime.Now.AddHours(2).AddMinutes(22), 25000, 25170, "ZE-XY123", "Zell am See", "München"));
+
+
+/*--------Das Selbe wie oben nur mit einem Schritt extra.---------*/
+Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(20), 25500, 25514, "ZE-XY123", "Zell am See", "Saalfelden");
+
+repository.Add(entrySaalfelden);
+/*----------------------------Ende-------------------------------*/
+
+
 List<Entry> entries = repository.GetAll();
 
 
