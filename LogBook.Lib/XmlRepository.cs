7bad9255608664518743c408c423edbb026f1414
Noah Teubner <nteubner@schueler.hakzell.at>
2024-02-05 12:18:17 +0100

    add create, delete and svae methods

--- LogBook.Lib/XmlRepository.cs
+++ LogBook.Lib/XmlRepository.cs
@@ -9,10 +9,12 @@ namespace LogBook.Lib;
 
 public class XmlRepository : IRepository
 {
-    XElement _rootElement;
+    readonly XElement _rootElement;
+    private readonly string _path;
 
     public XmlRepository(string path)
     {
+        _path = path;
         if (File.Exists(path))
         {
             _rootElement = XElement.Load(path);
@@ -24,12 +26,49 @@ public class XmlRepository : IRepository
     }
     public bool Add(Entry entry)
     {
-        throw new NotImplementedException();
+        XElement node = new("entry");
+
+        var idAttrib = new XAttribute("id", entry.Id.ToString());
+        node.Add(idAttrib);
+
+        var startAttrib = new XAttribute("start", entry.Start.ToString());
+        node.Add(startAttrib);
+
+        var endAttrib = new XAttribute("end", entry.End.ToString());
+        node.Add(endAttrib);
+
+        var startKmAttrib = new XAttribute("startkm", entry.StartKM.ToString());
+        node.Add(startKmAttrib);
+
+        var endKmAttrib = new XAttribute("endkm", entry.EndKM.ToString());
+        node.Add(endKmAttrib);
+
+        var numberPlateAttrib = new XAttribute("numberplate", entry.NumberPlate.ToString());
+        node.Add(numberPlateAttrib);
+
+        var fromAttrib = new XAttribute("from", entry.From.ToString());
+        node.Add(fromAttrib);
+
+        var toAttrib = new XAttribute("to", entry.To.ToString());
+        node.Add(toAttrib);
+
+        node.Add(entry.Description.ToString());
+
+        _rootElement.Add(node);
+
+        return this.Save();
+
     }
     
     public bool Delete(Entry entry)
     {
-        throw new NotImplementedException();
+        var itemsDel = from e in _rootElement.Descendants("etnry")
+                       where (string)e.Attribute("id") == entry.Id
+                       select e;
+
+        itemsDel.Remove();
+
+        return this.Save();
     }
 
     public List<Entry> GetAll()
@@ -48,7 +87,16 @@ public class XmlRepository : IRepository
 
     public bool Save()
     {
-        throw new NotImplementedException();
+        try
+        {
+            _rootElement.Save(_path);
+            return true;
+        }
+        catch(Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public bool Update(Entry entry)
