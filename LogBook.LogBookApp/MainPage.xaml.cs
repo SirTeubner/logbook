ed761773500ee81d4bacaa5cb6c53eb31c5521ac
Noah Teubner <nteubner@schueler.hakzell.at>
2024-02-08 08:28:53 +0100

    create mvvm (incl. binding)

--- LogBook.LogBookApp/MainPage.xaml.cs
+++ LogBook.LogBookApp/MainPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿namespace LogBook.LogBookApp
+﻿using LogBook.LogBookApp.ViewModels;
+
+namespace LogBook.LogBookApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
-        public MainPage()
+        public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            this.BindingContext = viewModel;
         }
     }
 
