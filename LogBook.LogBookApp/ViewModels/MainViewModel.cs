d20343d5e417d414b7d183bd4c7aa0106a0e62b9
Noah Teubner <nteubner@schueler.hakzell.at>
2024-02-19 12:05:53 +0100

    add ctor to vm

--- LogBook.LogBookApp/ViewModels/MainViewModel.cs
+++ LogBook.LogBookApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LogBook.Lib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace LogBook.LogBookApp.ViewModels;
 
-public partial class MainViewModel : ObservableObject
+public partial class MainViewModel(IRepository repository) : ObservableObject
 {
     public string Header => "Fahrtenbuch";
+    IRepository _repository = repository;
+
+
+
+
 }
