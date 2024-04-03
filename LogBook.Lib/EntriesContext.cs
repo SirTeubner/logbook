8a2ebe05a58048b82a8a24ebce4a65043e4f376c
Noah Teubner <nteubner@schueler.hakzell.at>
2024-04-03 13:09:32 +0200

    create sqliterepository and dbcontext

---
+++ LogBook.Lib/EntriesContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBook.Lib;
+
+public class EntriesContext : DbContext
+{
+    public DbSet<Entry> Entries { get; set; } // DbSet erstellt eine Tabelle in Sqlite welche wie unser <Entry> aufgebaut ist.
+
+    private string _path = string.Empty;
+
+    public EntriesContext(string path)
+    {
+        this._path = path;
+        SQLitePCL.Batteries_V2.Init();
+        this.Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        Debug.WriteLine(_path);
+
+        optionsBuilder.UseSqlite($"Filename={_path}");
+    }
+
+
+}
---
+++ LogBook.Lib/SqliteRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBook.Lib;
+
+public class SqliteRepository : IRepository
+{
+    string _path = string.Empty;
+    public SqliteRepository(string path)
+    {
+        this._path = path;   
+    }
+
+    public bool Add(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Entry? Find(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Entry> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+}
