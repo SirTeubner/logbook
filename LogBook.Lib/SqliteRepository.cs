3bf1f7384a30cc058d155c01c114aaee83dc60b2
Noah Teubner <nteubner@schueler.hakzell.at>
2024-04-04 08:32:50 +0200

    implemented sqlite repository

--- LogBook.Lib/SqliteRepository.cs
+++ LogBook.Lib/SqliteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,31 +17,153 @@ public class SqliteRepository : IRepository
 
     public bool Add(Entry entry)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using(var context = new EntriesContext(_path)) 
+            {
+                context.Add(entry);
+
+                context.SaveChanges();
+
+                return true;
+            }
+            
+            // return true
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
+
     }
 
     public bool Delete(Entry entry)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var context = new EntriesContext(_path))
+            {
+                context.Database.ExecuteSqlRaw("DELETE FROM Entries WHERE Id={0}",entry.Id);
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            // bei einem Fehler wir eine leere Liste zurückgegeben
+            // ev. wäre null besser ...
+            return false;
+        }
     }
 
     public Entry? Find(string id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var context = new EntriesContext(_path))
+            {
+                var find = (from entry in context.Entries
+                               where entry.Id == id
+                               select entry).FirstOrDefault(); // erstes Element oder null
+
+                return find;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            // bei einem Fehler wir eine leere Liste zurückgegeben
+            // ev. wäre null besser ...
+            return null;
+        }
     }
 
     public List<Entry> GetAll()
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var contetx = new EntriesContext(_path))
+            {
+                /*
+                var to = "saalfelden";
+
+                var entries = contetx.Entries.FromSql($"SELECT * From Entries WHERE `To`={to}").ToList();
+                */
+
+                var entries = contetx.Entries.FromSql($"SELECT * From Entries").ToList();
+
+                return entries;
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return new List<Entry>();
+        }
     }
+
+    /*
+    public List<Entry> GetAll()
+    {
+        try
+        {
+            using (var context = new EntriesContext(_path))
+            {
+               var entries = (from entry in context.Entries
+                             select entry).ToList();
+                return entries;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            // bei einem Fehler wir eine leere Liste zurückgegeben
+            // ev. wäre null besser ...
+            return new List<Entry>();
+        }
+    }
+    */
 
     public bool Save()
     {
-        throw new NotImplementedException();
+        // return true;
+        try
+        {
+            using (var context = new EntriesContext(_path))
+            {
+                context.SaveChanges();
+            }
+            return true;
+
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public bool Update(Entry entry)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var context = new EntriesContext(_path))
+            {
+                context.Entry(entry).State = EntityState.Modified; // der Entry wird in der Datenbank als verändert gespeichert.
+
+                context.SaveChanges();
+            }
+            return true;
+
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 }
